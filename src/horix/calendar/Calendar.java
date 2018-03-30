22983a82efe3f301d470a3d70f5d1d418e485c18
chae <chae@chae-PC>
2018-03-30 14:33:33 +0900

    월 입력 & 최대일수 출력하기

--- src/horix/calendar/Calendar.java
+++ src/horix/calendar/Calendar.java
@@ -1,5 +1,6 @@
 package horix.calendar;
 
+import java.util.Scanner;
 public class Calendar {
 	public static void main(String[] args) {
 		System.out.println(" 일   월    화    수    목    금    토");
@@ -8,6 +9,20 @@ public class Calendar {
 		System.out.println(" 8  9 10 11 12 13 14");
 		System.out.println("15 16 17 18 19 20 21");
 		System.out.println("22 23 24 25 26 27 28");
+		
+		//숫자를 입력 받아 해당하는 달의 최대 일수를 출력하는 프로그램
+		int[] month = {31,28,31,30,31,30,31,31,30,31,30,31};
+		System.out.println("원하는 월을 입력 하세요(1~12)");
+		
+		Scanner scanner = new Scanner(System.in);
+		/*String s1 = scanner.next();*/
+		
+		//int inputMonth = Integer.parseInt(s1);
+		int inputMonth = scanner.nextInt();
+		
+		System.out.printf("입력한 %d월은 %d일 까지 있습니다. ",inputMonth,month[inputMonth+1]);
+		
+		
 	}
 
 }
